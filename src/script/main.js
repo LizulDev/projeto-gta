8575c742cb8aa3a91202daf9e8ca8eb1f4eff461
LizulDev <luizfelipekauedasilva@gmail.com>
2024-01-29 16:02:09 -0300

    docs: creation of project 'main.js' and responsive.css

--- src/script/main.js
+++ src/script/main.js
@@ -1,2 +1,16 @@
-const myImage = document.querySelector("img");
+/*1o objetivo: ao clicar o botão plataformas, deve-se abrir naturalmente as plataformas que se deseja escolher;
+
+2o objetivo: Fechar a seleçai de plataformas quando o usuario clicar nela*/
+
+const btnPlataforma = document.querySelector(".btn-plataforma");
+const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+ 
+btnPlataforma.addEventListener("click", () => {
+    elementoPlataformas.classList.toggle("ativo");
+});
+
+
+
+
+
 
