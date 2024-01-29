faf0ec08e940792392dfeeb2118bae8f6c9a9e0d
LizulDev <luizfelipekauedasilva@gmail.com>
2024-01-29 16:04:11 -0300

    docs: cleaning comments

--- src/script/main.js
+++ src/script/main.js
@@ -1,7 +1,3 @@
-/*1o objetivo: ao clicar o botão plataformas, deve-se abrir naturalmente as plataformas que se deseja escolher;
-
-2o objetivo: Fechar a seleçai de plataformas quando o usuario clicar nela*/
-
 const btnPlataforma = document.querySelector(".btn-plataforma");
 const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
  
